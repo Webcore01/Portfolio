e742b96cce71ba1e13b4f6b59ef6f169618fa92f
Shivam singh <165698391+Webcore01@users.noreply.github.com>
2025-01-12 10:54:01 +0530

    upload updated file

---
+++ script.js
@@ -0,0 +1,11 @@
+document.querySelectorAll('nav ul li a').forEach(link => {
+    link.addEventListener('click', (e) => {
+        e.preventDefault();
+        const targetId = e.target.getAttribute('href').substring(1);
+        document.getElementById(targetId).scrollIntoView({
+            behavior: 'smooth',
+        });
+    });
+});
+
+
